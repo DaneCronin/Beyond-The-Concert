b089eb7d8a8888d6426689dd035d0c9828f529c4
Dane Cronin <danecronin@gmail.com>
2022-08-24 19:59:03 -0600

    added API keys to TicketMaster and LastFM

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
-var APIKeyBandsintown = "184f3c88-05d8-4d25-82d1-ba4db6cc27db"; //API Key for BandsinTown
+var APITicketMaster = "Bf30TtLUQxcKHdqHqQWR0a13lcphJbg5"; //TicketMaster API Key 
+var APILastFm = "64ea64681b70d9776ad3714be43dc7de"; //LastFM API Key
+
+fetch("https://app.ticketmaster.com/discovery/v2/events.json?city=Charlotte&apikey=" + APITicketMaster)
+.then (ticketMaster => {
+    console.log(ticketMaster);
+    return ticketMaster.json()
+}) 
+.then (ticketInfo => {
+    console.log(ticketInfo)
+});
+
+
+fetch("http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag=disco&api_key=" + APILastFm + "&format=json")
+.then (getLastFM  => {
+    console.log(getLastFM);
+    return getLastFM.json();
+})
+.then (getPopularArtist => {
+    console.log(getPopularArtist)
+});
