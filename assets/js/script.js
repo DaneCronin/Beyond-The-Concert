7a12c70095c5173cf36f391f235f069bfae475b6
Dane Cronin <danecronin@gmail.com>
2022-08-28 11:03:08 -0600

    added variables to script.js, modified id's for search fields in HTML.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,22 @@
+//API Key Variables
 var APITicketMaster = "Bf30TtLUQxcKHdqHqQWR0a13lcphJbg5"; //TicketMaster API Key 
 var APILastFm = "64ea64681b70d9776ad3714be43dc7de"; //LastFM API Key
 
+//Gloabl Variables 
+var date = new Date(data.dt * 1000);
+var day = date.getDate();
+var month = date.getMonth();
+var year = date.getFullYear();
+var artistNameSearch = document.querySelector("#artist-name-search"); //Variable for Artist Name search input field
+var genreTypeSearch = document.querySelector("#genre-type-search"); // Variable for the type of Genre searched.
+var locationSearched = document.querySelector("#location-searched"); // Variable for Location searched.
+var genreContainerEl = document.querySelector(".showsbygenre"); // Variable for container to hold returned shows by genre
+var showsTonightContainerEl = document.querySelector(".showstonight"); // Variable for container to hold returned shows for tonight. 
+
+
+
+
+//Fetch call to TicketMaster to get Event data for dates, venues
 fetch("https://app.ticketmaster.com/discovery/v2/events.json?city=Charlotte&apikey=" + APITicketMaster)
 .then (ticketMaster => {
     console.log(ticketMaster);
@@ -10,7 +26,7 @@ fetch("https://app.ticketmaster.com/discovery/v2/events.json?city=Charlotte&apik
     console.log(ticketInfo)
 });
 
-
+//Fetch to LastFM to get Artist info/popular songs/
 fetch("http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag=disco&api_key=" + APILastFm + "&format=json")
 .then (getLastFM  => {
     console.log(getLastFM);
