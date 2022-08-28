e14c0388a3e51d1f7f2d6c4527d2fc2ae3b170d0
sam conteh <sambujangconteh4@gmail.com>
2022-08-28 18:06:29 -0400

    add apikeys for artist info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 var APITicketMaster = "Bf30TtLUQxcKHdqHqQWR0a13lcphJbg5"; //TicketMaster API Key 
 var APILastFm = "64ea64681b70d9776ad3714be43dc7de"; //LastFM API Key
+var band = []
+var count = 0
 
 fetch("https://app.ticketmaster.com/discovery/v2/events.json?city=Charlotte&apikey=" + APITicketMaster)
 .then (ticketMaster => {
@@ -19,3 +21,58 @@ fetch("http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag=disco&api_
 .then (getPopularArtist => {
     console.log(getPopularArtist)
 });
+
+let LastFM0 = 'https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + band[0] + '&api_key=78661b1408a61c6f77a83efc09f78da4&format=json'
+fetch(LastFM0)
+    .then(
+        function (response) {
+            return response.json();
+        })
+    .then(data => {
+        $(".artistInfoResults0").empty();
+        var artistInfo = data.artist.bio.summary
+        $(".artistInfoResults0").text(artistInfo)
+    })
+let LastFM1 = 'https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + band[1] + '&api_key=78661b1408a61c6f77a83efc09f78da4&format=json'
+fetch(LastFM1)
+    .then(
+        function (response) {
+            return response.json();
+        })
+    .then(data => {
+        var artistInfo = data.artist.bio.summary
+        $(".artistInfoResults1").text(artistInfo)
+    })
+let LastFM2 = 'https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + band[2] + '&api_key=78661b1408a61c6f77a83efc09f78da4&format=json'
+fetch(LastFM2)
+    .then(
+        function (response) {
+            return response.json();
+        })
+    .then(data => {
+        var artistInfo = data.artist.bio.summary
+        $(".artistInfoResults2").append(artistInfo)
+    })
+let LastFM3 = 'https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + band[3] + '&api_key=78661b1408a61c6f77a83efc09f78da4&format=json'
+fetch(LastFM3)
+    .then(
+        function (response) {
+            return response.json();
+        })
+    .then(data => {
+        var artistInfo = data.artist.bio.summary
+        $(".artistInfoResults3").append(artistInfo)
+    })
+let LastFM4 = 'https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + band[4] + '&api_key=78661b1408a61c6f77a83efc09f78da4&format=json'
+fetch(LastFM4)
+    .then(
+        function (response) {
+            return response.json();
+        })
+    .then(data => {
+        var artistInfo = data.artist.bio.summary
+        $(".artistInfoResults4").append(artistInfo)
+    });
+
+
+
