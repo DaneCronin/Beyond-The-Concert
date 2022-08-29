1f8ea52cd2c538dbfedc4875feedd8c46d177c09
sukipannu <sukipannu@ymail.com>
2022-08-29 12:14:16 -0400

    updated script

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,8 @@ var APIKeyBandsintown = "184f3c88-05d8-4d25-82d1-ba4db6cc27db"; //API Key for Ba
 // variables for search button
 var searchBtn = document.querySelector("search-button");
 
-// variables for artists
+// variables for shows tonight
+var showsTonight = document.querySelector("#shows-tonight")
 
 // variables for genre
 
